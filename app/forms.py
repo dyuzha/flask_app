ca40e614cdd124bf9b968d90018673f1e5796898
dyuzha <matvey10d@gmail.com>
2025-04-23 10:57:31 +0300

    fix bag with login form

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,7 @@
-from flask.ext.wtf import Form
-from wtforms import TextField, BooleanField
-from wtforms.validators import Required
+from flask_wtf import FlaskForm
+from wtforms import StringField, BooleanField
+from wtforms.validators import DataRequired
 
-class LoginForm(Form):
-    openid = TextField('openid', validators = [Required()])
+class LoginForm(FlaskForm):
+    openid = StringField('openid', validators = [DataRequired()])
     remember_me = BooleanField('remember_me', default = False)
