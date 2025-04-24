1faf03f73e9c97652c83b88257eb80997adbeeed
dyuzha <matvey10d@gmail.com>
2025-04-24 15:43:13 +0300

    refactoring

--- app/routes.py
+++ app/routes.py
@@ -27,15 +27,11 @@ def index():
         posts = posts)
 
 
-# функция представления index опущена для краткости
-
 @app.route('/login', methods = ['GET', 'POST'])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for OpenID="' + form.openid.data + '", remember_me=' + str(form.remember_me.data))
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
         return redirect('/index')
-    return render_template('login.html',
-        title = 'Sign In',
-        form = form,
-        providers = app.config['OPENID_PROVIDERS'])
+    return render_template('login.html', title='Sign In', form=form)
