8abf7ffc3030c3e0588fa4a12af8eb4ebc9b9707
dyuzha <matvey10d@gmail.com>
2025-04-24 11:51:20 +0300

    refactoring

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,9 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from config import Config
 
 app = Flask(__name__)
-app.config.from_object('config')
+app.config.from_object(Config)
 db = SQLAlchemy(app)
 
-from app import views, models
+from app import routes, models
