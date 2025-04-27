5268f73254a02010b476c72de0b78af8bd893829
dyuzha <matvey10d@gmail.com>
2025-04-27 15:41:34 +0300

    add flask_context_shell setting

--- flask_app.py
+++ flask_app.py
@@ -1,3 +1,12 @@
-from app import app
+import sqlalchemy as sa
+import sqlalchemy.orm as so
+from app import app, db
+from app.models import User, Post
+
+# Настройка контекста оболочки
+@app.shell_context_processor
+def make_shell_context():
+    return {'sa': sa, 'so': so, 'db': db, 'User': User, 'Post': Post}
+
 if __name__ == "__main__":
     app.run(debug=True)
