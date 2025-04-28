70956f3593e37692a2cabbe0a9178b3ac45801d0
dyuzha <matvey10d@gmail.com>
2025-04-28 18:57:28 +0300

    add future login compleet

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,10 @@
-from flask import render_template, flash, redirect, url_for
-from flask_login import current_user, login_user, logout_user
+from flask import render_template, flash, redirect, url_for, request
+from flask_login import current_user, login_user, logout_user, login_required
 import sqlalchemy as sa
 from app.models import User
 from app import app, db
 from app.forms import LoginForm
+from urllib.parse import urlsplit
 """
 Представления - обработчики, которые отвечают на запросы веб-браузера,
 Представления в Flask пишутся как Python функции.
@@ -12,8 +13,9 @@ from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
-    user = { 'nickname': 'Miguel' } # выдуманный пользователь
+    # user = { 'nickname': 'Miguel' } # выдуманный пользователь
     posts = [ # список выдуманных постов
         {
             'author': { 'nickname': 'John' },
@@ -26,7 +28,7 @@ def index():
     ]
     return render_template("index.html",
         title = 'Home',
-        user = user,
+        # user = user,
         posts = posts)
 
 
@@ -42,8 +44,12 @@ def login():
         if user is None or not user.check_password(form.password.data):
             flash('Invalid username or password')
             return redirect(url_for('login'))
+
         login_user(user, remember=form.remember_me.data)
-        return redirect(url_for('index'))
+        next_page = request.args.get('next')
+        if not next_page or urlsplit(next_page).netloc != "":
+            next_page = url_for('index')
+        return redirect(next_page)
 
     return render_template('login.html', title='Sign In', form=form)
 
