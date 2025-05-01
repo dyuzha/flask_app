3cf7fe5db5aee300a51ef4ba334baf3fa59adf7e
dyuzha <matvey10d@gmail.com>
2025-05-01 12:35:42 +0300

    add future eddit profile

--- app/routes.py
+++ app/routes.py
@@ -3,19 +3,39 @@ from flask_login import current_user, login_user, logout_user, login_required
 import sqlalchemy as sa
 from app.models import User
 from app import app, db
-from app.forms import LoginForm, RegistrationForm
+from app.forms import LoginForm, RegistrationForm, EditProfileForm
 from urllib.parse import urlsplit
+from datetime import datetime, timezone
 """
 Представления - обработчики, которые отвечают на запросы веб-браузера,
 Представления в Flask пишутся как Python функции.
 Каждая функция представления сопоставляется с одним или несколькими URL
 """
 
+@app.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    """Страница редактирования профиля"""
+    form = EditProfileForm() # Если данные прошли валидацию, то они обовляются в бд
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash('Your changes have been saved.')
+        return redirect(url_for('edit_profile'))
+    # Если это GET запрос, значит пользователь еще не редактировал профиль
+    # Поля заполняются текущими данными из б
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', title='Edit Profile', form=form)
+
+
 @app.route('/')
 @app.route('/index')
 @login_required
 def index():
-    # user = { 'nickname': 'Miguel' } # выдуманный пользователь
+    """Главная страница сайта"""
     posts = [ # список выдуманных постов
         {
             'author': { 'nickname': 'John' },
@@ -30,9 +50,12 @@ def index():
         title = 'Home Page',
         posts = posts)
 
+
 @app.route('/user/<username>')
 @login_required
 def user(username):
+    """Страница пользователя"""
+    # Если пользователь не существует - возвращает страницу 404
     user = db.first_or_404(sa.select(User).where(User.username == username))
     posts = [ {'author': user, 'body': 'Test post #1'},
              {'author': user, 'body': 'Test post #2'} ]
@@ -42,9 +65,10 @@ def user(username):
 
 @app.route('/login', methods = ['GET', 'POST'])
 def login():
+    """Страница входа"""
+    # Если пользователь зарегистрирован, то он перенаправляется на гл. страницу
     if current_user.is_authenticated:
         return redirect(url_for('index'))
-
     form = LoginForm()
     if form.validate_on_submit():
         user = db.session.scalar(
@@ -64,12 +88,23 @@ def login():
 
 @app.route('/logout')
 def logout():
+    """Выход пользователя из системы и перенаправлеие на гл. страницу"""
     logout_user()
     return redirect(url_for('index'))
+
+@app.before_request
+def before_request():
+    """Выполняется перед каждым посещением любой страницы"""
+    # Если пользователь авторизовае,
+    if current_user.is_authenticated:
+        # текущее время записывается в бд, как last_seen
+        current_user.last_seen = datetime.now(timezone.utc)
+        db.session.commit()
 
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
+    """Страница регистрации пользователя"""
     if current_user.is_authenticated:
         return redirect(url_for('index'))
     form = RegistrationForm()
