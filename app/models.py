6efb6701ffdfe1380c68fa7361f772a4a6f6f69e
dyuzha <matvey10d@gmail.com>
2025-05-01 23:47:03 +0300

    Добавил связю многие ко многим между пользователями (подписчики, подписки)
    Создал ассоциативную таблицу followers

--- app/models.py
+++ app/models.py
@@ -13,6 +13,15 @@ from hashlib import md5
 #     email = db.Column(db.String(120), index = True, unique = True)
 #     password_hash = db.Column(db.String(128))
 
+followers = sa.Table(
+    'followers',
+    db.metadata,
+    sa.Column('followers_id', sa.Integer, sa.ForeignKey('user.id'),
+              primary_key=True),
+    sa.Column('followed_id', sa.Integer, sa.ForeignKey('user.id'),
+              primary_key=True)
+    )
+
 
 class User(UserMixin, db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
@@ -26,6 +35,18 @@ class User(UserMixin, db.Model):
     about_me: so.Mapped[Optional[str]] = so.mapped_column(sa.String(140))
     last_seen: so.Mapped[Optional[datetime]] = so.mapped_column(
             default=lambda: datetime.now(timezone.utc))
+
+    following: so.WriteOnlyMapped['User'] = so.relationship(
+            secondary=followers,
+            primaryjoin=(followers.c.follower_id == id),
+            secondaryjoin=(followers.c.followed_id == id),
+            back_populates='followers')
+
+    followers: so.WriteOnlyMapped['User'] = so.relationship(
+            secondary=followers,
+            primaryjoin=(followers.c.followed_id == id),
+            secondaryjoin=(followers.c.follower_id == id),
+            back_populates='following')
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
