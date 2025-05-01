e14439e7f579afb14af523f51653b21fb5caada0
dyuzha <matvey10d@gmail.com>
2025-05-01 17:11:29 +0300

    Add future mail logger

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,8 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+import logging
+from logging.handlers import SMTPHandler
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -10,5 +12,35 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
+
+def run_mail_logger():
+    """Запускает режим отладки по электронной почте"""
+    # Если указаны параметры авторизации, исопльзует авторизацию
+    if app.config['MAIL_USERNAME'] and app.config['MAIL_PASSWORD']:
+        auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
+    else:
+        auth = None
+
+    # Если параметр шифрования - True, использует шифрование
+    if app.config['MAIL_USE_TLS']:
+        secure = ()
+    else:
+        secure = None
+
+    # Создание обработчика
+    mail_handler = SMTPHandler(
+        mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
+        fromaddr='no-reply@' + app.config['MAIL_SERVER'],
+        toaddrs=app.config['ADMINS'],
+        subject='Microblog Failure', # Тема письма
+        credentials=auth, secure=secure)
+
+    # Подключение к логеру фласк
+    mail_handler.setLevel(logging.ERROR)
+    app.logger.addHandler(mail_handler)
+
+# Если запущен в продакшене и есть конфигурация сервера
+if not app.debug and app.config['MAIL_SERVER']:
+    run_mail_logger()
 
 from app import routes, models, errors
--- config.py
+++ config.py
@@ -11,5 +11,11 @@ class Config:
             'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    # Директория с миграционными файлами
-    # SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+    # Mail-settings
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['matvey_10d@gmail.com']
+
